3c9538211c1fb8d2aa08e516718cf5cbaa943db4
ZhaoJohnson <Johnson.zhao@starlight-sms.com>
2016-04-15 04:51:52 -0500

    New Works
    Signed-off-by: ZhaoJohnson <Johnson.zhao@starlight-sms.com>

---
+++ publishToolsTwo/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace publishToolsTwo
+{
+    partial class form_main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.projectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.command1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.userToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.goToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.systemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pd_desktop = new System.Windows.Forms.Panel();
+            this.tc_desktop = new System.Windows.Forms.TabControl();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.pd_desktop.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.projectToolStripMenuItem,
+            this.runToolStripMenuItem,
+            this.systemToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(563, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "ms_Star";
+            // 
+            // projectToolStripMenuItem
+            // 
+            this.projectToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem});
+            this.projectToolStripMenuItem.Name = "projectToolStripMenuItem";
+            this.projectToolStripMenuItem.Size = new System.Drawing.Size(60, 21);
+            this.projectToolStripMenuItem.Text = "Project";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.command1ToolStripMenuItem,
+            this.userToolStripMenuItem});
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            // 
+            // command1ToolStripMenuItem
+            // 
+            this.command1ToolStripMenuItem.Name = "command1ToolStripMenuItem";
+            this.command1ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.command1ToolStripMenuItem.Text = "Defult";
+            this.command1ToolStripMenuItem.Click += new System.EventHandler(this.command1ToolStripMenuItem_Click);
+            // 
+            // userToolStripMenuItem
+            // 
+            this.userToolStripMenuItem.Name = "userToolStripMenuItem";
+            this.userToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.userToolStripMenuItem.Text = "Browser";
+            this.userToolStripMenuItem.Click += new System.EventHandler(this.userToolStripMenuItem_Click);
+            // 
+            // runToolStripMenuItem
+            // 
+            this.runToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.goToolStripMenuItem});
+            this.runToolStripMenuItem.Name = "runToolStripMenuItem";
+            this.runToolStripMenuItem.Size = new System.Drawing.Size(42, 21);
+            this.runToolStripMenuItem.Text = "Run";
+            // 
+            // goToolStripMenuItem
+            // 
+            this.goToolStripMenuItem.Name = "goToolStripMenuItem";
+            this.goToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.goToolStripMenuItem.Text = "Go";
+            this.goToolStripMenuItem.Click += new System.EventHandler(this.goToolStripMenuItem_Click);
+            // 
+            // systemToolStripMenuItem
+            // 
+            this.systemToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeToolStripMenuItem});
+            this.systemToolStripMenuItem.Name = "systemToolStripMenuItem";
+            this.systemToolStripMenuItem.Size = new System.Drawing.Size(61, 21);
+            this.systemToolStripMenuItem.Text = "System";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // pd_desktop
+            // 
+            this.pd_desktop.Controls.Add(this.tc_desktop);
+            this.pd_desktop.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pd_desktop.Location = new System.Drawing.Point(0, 25);
+            this.pd_desktop.Name = "pd_desktop";
+            this.pd_desktop.Size = new System.Drawing.Size(563, 382);
+            this.pd_desktop.TabIndex = 1;
+            // 
+            // tc_desktop
+            // 
+            this.tc_desktop.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tc_desktop.Location = new System.Drawing.Point(0, 0);
+            this.tc_desktop.Name = "tc_desktop";
+            this.tc_desktop.SelectedIndex = 0;
+            this.tc_desktop.Size = new System.Drawing.Size(563, 382);
+            this.tc_desktop.TabIndex = 2;
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(47, 21);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // form_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 407);
+            this.Controls.Add(this.pd_desktop);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "form_main";
+            this.Text = "PublishTools .v2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.pd_desktop.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem projectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem runToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem goToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem systemToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.Panel pd_desktop;
+        private System.Windows.Forms.ToolStripMenuItem command1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem userToolStripMenuItem;
+        private System.Windows.Forms.TabControl tc_desktop;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+    }
+}
+
---
+++ publishToolsTwo/Form1.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using ICSharpCode.SharpZipLib;
+using ICSharpCode.TextEditor;
+using System.Diagnostics;
+
+namespace publishToolsTwo
+{
+    public partial class form_main : Form
+    {
+        public form_main()
+        {
+            InitializeComponent();
+        }
+
+        public string OpenPath { get; set; }
+        const int CLOSE_SIZE = 12;
+        public static bool getdate = false;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.tc_desktop.TabPages.Clear();
+            this.tc_desktop.DrawMode = TabDrawMode.OwnerDrawFixed;
+            this.tc_desktop.Padding = new System.Drawing.Point(CLOSE_SIZE, CLOSE_SIZE);
+            this.tc_desktop.DrawItem += Tc_desktop_DrawItem;
+            this.tc_desktop.MouseDown += Tc_desktop_MouseDown;
+        }
+
+      
+
+        private void command1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string sourcePath = Application.StartupPath;
+            OpenPath = sourcePath + @"\Defult.ini";
+            
+            //defaultBuilder.AppendLine("");
+            //defaultBuilder.AppendLine(";Set to delete empty folders");
+            //defaultBuilder.AppendLine(";Ture Need configuration settings True/False");
+            //defaultBuilder.AppendLine(";Example: key=False");
+            //defaultBuilder.AppendLine("[Deletefolder]");
+            //defaultBuilder.AppendLine("Deletefolder=True");
+
+            if (!File.Exists(OpenPath))
+            {
+                StringBuilder defaultBuilder = new StringBuilder();
+                defaultBuilder.AppendLine(";Sets the path to the folder needs to be cleaned");
+                defaultBuilder.AppendLine(";Need to configure the full path name");
+                defaultBuilder.AppendLine(";Example: key = D:");
+                defaultBuilder.AppendLine("[Path]");
+                defaultBuilder.AppendLine("Path_1=D:\\folder");
+                defaultBuilder.AppendLine("");
+                defaultBuilder.AppendLine(";Set the file type needs to be filtered");
+                defaultBuilder.AppendLine(";Need to configure the full suffix");
+                defaultBuilder.AppendLine(";Example: key=.exe");
+                defaultBuilder.AppendLine("[Filter]");
+                defaultBuilder.AppendLine("Filter_1=.xml");
+                defaultBuilder.AppendLine("filter_2=.pdb");
+                defaultBuilder.AppendLine("");
+                defaultBuilder.AppendLine(";Setup requires only the files required to keep");
+                defaultBuilder.AppendLine(";Need to configure the full name Or Null");
+                defaultBuilder.AppendLine(";Example: key=setup.exe");
+                defaultBuilder.AppendLine("[includefile]");
+                defaultBuilder.AppendLine("include=api.dll");
+                defaultBuilder.AppendLine("");
+                defaultBuilder.AppendLine(";Setup requires only the files required to keep");
+                defaultBuilder.AppendLine(";Need to configure the full name Or Null");
+                defaultBuilder.AppendLine(";Example: key=windows");
+                defaultBuilder.AppendLine("[includefolder]");
+                defaultBuilder.AppendLine("includefolder=api.dll");
+                defaultBuilder.AppendLine("");
+                defaultBuilder.AppendLine(";Setting Deletes the specified file");
+                defaultBuilder.AppendLine(";Need to configure the full name Or Null");
+                defaultBuilder.AppendLine(";Example: key=fileName.dll");
+                defaultBuilder.AppendLine("[DelFile]");
+                defaultBuilder.AppendLine("DelFile=fileName.dll");
+                defaultBuilder.AppendLine("");
+                defaultBuilder.AppendLine(";Setting Deletes the specified Folder");
+                defaultBuilder.AppendLine(";Need to configure the full Path Or Null");
+                defaultBuilder.AppendLine(";Example: key=folderName");
+                defaultBuilder.AppendLine("[DelFolder]");
+                defaultBuilder.AppendLine("DelFolder=folderName");
+                defaultBuilder.AppendLine("");
+                defaultBuilder.AppendLine(";Set the output path of the zip file");
+                defaultBuilder.AppendLine(";Need to configure the file name and full address");
+                defaultBuilder.AppendLine(";Example: key=D:\\test\\setup.zip");
+                defaultBuilder.AppendLine("[OutPath]");
+                defaultBuilder.AppendLine("outPath=D:\\Sbin.zip");
+                FileStream myfs = new FileStream(OpenPath, FileMode.Create);
+                StreamWriter mysw = new StreamWriter(myfs);
+                mysw.Write(defaultBuilder.ToString());
+                mysw.Flush();
+                mysw.Close();
+                myfs.Close();
+            }
+            this.tc_desktop.TabPages.Add(CreateProject(OpenPath));
+        }
+
+        private TabPage CreateProject(string path)
+        {
+            TabPage newpage = new TabPage(); //新增一个PAGE
+            newpage.Text = Path.GetFileNameWithoutExtension(path);
+            newpage.ToolTipText = path;
+            TextEditorControl nbTextEdit = new TextEditorControl
+            {
+                Dock = DockStyle.Fill,
+                Parent = newpage,
+                Text = File.ReadAllText(path)
+        };
+            newpage.Controls.Add(nbTextEdit);
+            return newpage;
+        }
+
+        private void userToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog dg = new OpenFileDialog();
+            dg.Filter = "ini files(*.ini)|*.ini";
+            dg.FilterIndex = 1;
+            dg.RestoreDirectory = true;
+            dg.InitialDirectory = Application.StartupPath;
+            if (dg.ShowDialog() == DialogResult.OK)
+            {
+                this.tc_desktop.TabPages.Add(CreateProject(dg.FileName));
+            }
+
+        }
+
+        private void goToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string scOutput = "";
+            //bool deleteFolder = false;
+
+            TabPage currentPage = this.tc_desktop.SelectedTab;
+            if (currentPage == null) return;
+            SaveIniFile(currentPage);
+            StringCollection scPath = new StringCollection();
+            StringCollection scFilter = new StringCollection();
+            StringCollection scInclued = new StringCollection();
+            StringCollection scdelfiles = new StringCollection();
+            StringCollection scdelfolders = new StringCollection();
+            ReadIniFile(ref scOutput, currentPage, scPath, scFilter, scInclued, scdelfiles, scdelfolders);
+            Working(scPath, scFilter, scInclued, scdelfiles, scdelfolders, scOutput);
+
+            System.Diagnostics.Process.Start("Explorer.exe", $"/select,{scOutput}");
+        }
+
+        private static void ReadIniFile(ref string scOutput, TabPage currentPage, StringCollection scPath, StringCollection scFilter, StringCollection scInclued,StringCollection scdelfiles, StringCollection scdelfolders)
+        {
+            IniFiles iniFiles = new IniFiles(currentPage.ToolTipText);
+            //读取所有Section
+            StringCollection inisection = new StringCollection();
+            iniFiles.ReadSections(inisection);
+            foreach (string s1 in inisection)
+            {
+                //Section内的所有KEY
+                StringCollection mycs = new StringCollection();
+                iniFiles.ReadSection(s1, mycs);
+                foreach (string myc in mycs)
+                {
+                    switch (s1)
+                    {
+                        case "Path":
+                            scPath.Add(iniFiles.ReadString(s1, myc, ""));
+                            break;
+                        case "Filter":
+                            scFilter.Add(iniFiles.ReadString(s1, myc, ""));
+                            break;
+                        case "includefile":
+                            scInclued.Add(iniFiles.ReadString(s1, myc, ""));
+                            break;
+                        case "includefolder":
+                            scInclued.Add(iniFiles.ReadString(s1, myc, ""));
+                            break;
+                        case "DelFile":
+                            scdelfiles.Add(iniFiles.ReadString(s1, myc, ""));
+                            break;
+                        case "DelFolder":
+                            scdelfolders.Add(iniFiles.ReadString(s1, myc, ""));
+                            break;
+                        case "OutPath":
+                            scOutput = iniFiles.ReadString(s1, myc, "");
+                            break;
+                        case "GetDate":
+                            getdate = true;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
+        private static void SaveIniFile(TabPage currentPage)
+        {
+            TextEditorControl nbTextEdit = currentPage.Controls[0] as TextEditorControl;
+            File.WriteAllText(currentPage.ToolTipText, nbTextEdit.Text);
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Working(StringCollection scpath, StringCollection scFilter, StringCollection scInclude, StringCollection scdelfiles, StringCollection scdelfolders
+            ,string Output)
+        {
+            clearFile(scpath, scFilter, scInclude, scdelfiles, scdelfolders);
+
+            if (!string.IsNullOrEmpty(Output))
+            {
+                foreach (string path in scpath)
+                {
+                    ZipHelper.CreateZip(path, Output);
+                }
+            }
+        }
+        
+        public void clearFile(StringCollection scpath, StringCollection scFilter, StringCollection scInclude, StringCollection scdelfiles, StringCollection scdelfolders)
+        {
+            foreach (string path in scpath)
+            {
+                DirectoryInfo dir = new DirectoryInfo(path);
+                if (dir.Exists)
+                {
+                    if (scInclude.Count > 0)
+                    {
+                        foreach (string include in scInclude)
+                        {
+                            foreach (FileInfo fileinclude in dir.GetFiles(include, SearchOption.AllDirectories))
+                            {
+                                fileinclude.Attributes = FileAttributes.ReadOnly;
+                            }
+
+                        }
+                            foreach (FileInfo file in dir.GetFiles("*.*", SearchOption.AllDirectories))
+                            {
+                                if (file.Attributes != FileAttributes.ReadOnly)
+                                {
+                                    file.Delete();
+                                }
+                            }
+                    }
+                    else
+                    {
+                        if (scdelfiles.Count > 0)
+                        {
+                            foreach (string scdelfile in scdelfiles)
+                            {
+                                foreach (FileInfo delfile in dir.GetFiles(scdelfile, SearchOption.AllDirectories))
+                                {
+                                    delfile.Delete();
+                                }
+
+                            }
+                        }
+                        if (scdelfolders.Count > 0)
+                        {
+                            foreach (string scdelfolder in scdelfolders)
+                            {
+                                foreach (DirectoryInfo directory in dir.GetDirectories(scdelfolder))
+                                {
+                                    directory.Delete();
+                                }
+                            }
+                        }
+                        foreach (FileInfo fileInfo in dir.GetFiles("*.*", SearchOption.AllDirectories))
+                        {
+                            foreach (string ft in scFilter)
+                            {
+                                if (fileInfo.Extension.ToUpper() == ft.ToUpper())
+                                {
+                                    fileInfo.Delete();
+                                }
+                            }
+                        }
+                    }
+                        foreach (DirectoryInfo directory in dir.GetDirectories())
+                        {
+                            if (!(directory.GetFiles().Length > 0))
+                            {
+                                directory.Delete();
+                            }
+                        }
+                }
+            }
+        }
+
+        //添加Tabpage关闭按钮
+        private void Tc_desktop_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                int x = e.X, y = e.Y;
+                //计算关闭区域   
+                Rectangle myTabRect = this.tc_desktop.GetTabRect(this.tc_desktop.SelectedIndex);
+                myTabRect.Offset(myTabRect.Width - (CLOSE_SIZE + 3), 2);
+                myTabRect.Width = CLOSE_SIZE;
+                myTabRect.Height = CLOSE_SIZE;
+                //如果鼠标在区域内就关闭选项卡   
+                bool isClose = x > myTabRect.X && x < myTabRect.Right
+                 && y > myTabRect.Y && y < myTabRect.Bottom;
+                if (isClose == true)
+                {
+                    SaveIniFile(this.tc_desktop.SelectedTab);
+                    this.tc_desktop.TabPages.Remove(this.tc_desktop.SelectedTab);
+                }
+            }
+        }
+
+        private void Tc_desktop_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            try
+            {
+                Rectangle myTabRect = this.tc_desktop.GetTabRect(e.Index);
+                //先添加TabPage属性   
+                e.Graphics.DrawString(this.tc_desktop.TabPages[e.Index].Text
+                , this.Font, SystemBrushes.ControlText, myTabRect.X + 2, myTabRect.Y + 2);
+                //再画一个矩形框
+                using (Pen p = new Pen(Color.Black))
+                {
+                    myTabRect.Offset(myTabRect.Width - (CLOSE_SIZE + 3), 2);
+                    myTabRect.Width = CLOSE_SIZE;
+                    myTabRect.Height = CLOSE_SIZE;
+                    e.Graphics.DrawRectangle(p, myTabRect);
+                }
+                //填充矩形框
+                Color recColor = e.State == DrawItemState.Selected ? Color.DarkRed : Color.DarkGray;
+                using (Brush b = new SolidBrush(recColor))
+                {
+                    e.Graphics.FillRectangle(b, myTabRect);
+                }
+                //画关闭符号
+                using (Pen p = new Pen(Color.White))
+                {
+                    //画"/"线
+                    Point p1 = new Point(myTabRect.X + 3, myTabRect.Y + 3);
+                    Point p2 = new Point(myTabRect.X + myTabRect.Width - 3, myTabRect.Y + myTabRect.Height - 3);
+                    e.Graphics.DrawLine(p, p1, p2);
+                    //画"/"线
+                    Point p3 = new Point(myTabRect.X + 3, myTabRect.Y + myTabRect.Height - 3);
+                    Point p4 = new Point(myTabRect.X + myTabRect.Width - 3, myTabRect.Y + 3);
+                    e.Graphics.DrawLine(p, p3, p4);
+                }
+                e.Graphics.Dispose();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TabPage currentPage = this.tc_desktop.SelectedTab;
+            if (currentPage == null) return;
+            SaveIniFile(currentPage);
+        }
+    }
+}
